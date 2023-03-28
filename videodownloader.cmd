2e1b300da1dcbaa116565535241399776b9d6d01
2stinkysocks <54291521+2stinkysocks@users.noreply.github.com>
2023-03-28 14:00:40 -0700

    force audio and video

--- videodownloader.cmd
+++ videodownloader.cmd
@@ -6,6 +6,6 @@ echo ###############################################
 
 set /p "url=Video URL > "
 set /p "name=Output Filename > "
-powershell -command "%APPDATA%\nvm\v19.8.1\npx.cmd ytdl %url% -o %USERPROFILE%\Downloads\%name%"
+powershell -command "%APPDATA%\nvm\v19.8.1\npx.cmd ytdl %url% --filter audioandvideo -o %USERPROFILE%\Downloads\%name%"
 echo Saved to Downloads
 pause >nul
